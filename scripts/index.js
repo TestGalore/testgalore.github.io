b3a244c32b07d93a1af6b2b972544b2d85457fba
ElasticBop <elasticbop@gmail.com>
2023-05-07 16:41:10 -0700

    Setup basic functionality to increment projects

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+
 document.addEventListener('DOMContentLoaded', function() {
     window.addEventListener('scroll', function() {
         let scroll = window.scrollY;
@@ -10,3 +11,25 @@ document.addEventListener('DOMContentLoaded', function() {
 
     });
 });
+
+let currentProject = 0;
+let numProjects = 3;
+
+let projectContainer = document.getElementById("project-container");
+let projects = document.getElementsByClassName("project-item");
+
+let style = window.getComputedStyle(projectContainer);
+let scrollAmountTillNextProject = window.innerWidth - parseInt(style.getPropertyValue("gap"));
+console.log(projectContainer.scrollWidth);
+
+let counter = 0
+window.addEventListener('click', function() {
+
+    projectContainer.scrollTo({
+        top: 0,
+        left: scrollAmountTillNextProject*counter,
+        behaviour: "smooth",
+    });
+    counter = ++counter % numProjects;
+})
+

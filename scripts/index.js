38bc458c21e392f3c895978a192972ac08f55720
ElasticBop <elasticbop@gmail.com>
2023-05-06 20:49:50 -0700

    Changed styling for scrollbar

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
     window.addEventListener('scroll', function() {
-        var scroll = window.scrollY;
-        var arrow = document.querySelector('.arrow');
+        let scroll = window.scrollY;
+        let arrow = document.querySelector('.arrow');
         if (scroll >= 1) {
             arrow.classList.add('fade');
         } else{
             arrow.classList.remove('fade');
         }
+        
     });
 });

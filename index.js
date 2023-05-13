0f8db8d312a21bdd56a3bcceff81cb597e03b840
ElasticBop <elasticbop@gmail.com>
2023-05-13 08:56:19 -0700

    Added project links

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 
 //global vars
-let projectList = [{
-    description : "Response Portfolio. Built using HTML, CSS, and Javascript.",
-    link : "https://elasticbop.github.io/projects/Pathfinding/index.html",
-    img : "./images/project1.PNG"
-}, {
-    description : "A-star Visualizer. Built using HTML, CSS, and Javascript",
-    link : "https://elasticbop.github.io/projects/Pathfinding/index.html",
-    img : "./images/project2.PNG"        
-}
+let projectList = 
+[ 
+    {
+        name: "A-Star Visualizer",
+        description : "Built using HTML, CSS, and Javascript",
+        codeLink : "https://github.com/ElasticBop/A-Star-Visualizer",
+        projLink : "https://elasticbop.github.io/projects/Pathfinding/index.html",
+        img : "./images/astar.PNG"        
+    },
+    {
+        name: "Portfolio",
+        description : "Responsive. Built with HTML, CSS, and Javascript.",
+        codeLink : "https://github.com/ElasticBop/elasticbop.github.io",
+        projLink : "https://elasticbop.github.io",
+        img : "./images/portfolio.PNG"
+    }
 ];
+
 let projectContainer = document.getElementById("project-container");
 let style = window.getComputedStyle(projectContainer);
 let scrollAmountTillNextProject = window.innerWidth - parseInt(style.getPropertyValue("gap"));
@@ -34,59 +42,6 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-//computes values provided that projects are loaded 
-function computeValues(){
-    scrollAmountTillNextProject = window.innerWidth - parseInt(style.getPropertyValue("gap"));
-    projectItemWidth = projects[0].offsetWidth;
-    projectContainerLeft = window.innerWidth/2 - projectItemWidth/2 ;
-    projectContainerRight = window.innerWidth/2 + projectItemWidth/2;
-}
-
-function addProject(project){
-    let pitem = document.createElement("div");
-    let pimagecon = document.createElement("div");
-    let pimage = document.createElement("img");
-    let ptextcon = document.createElement("div");
-    let pspan = document.createElement("span");
-    pspan.appendChild(document.createTextNode(project.description));
-    ptextcon.setAttribute("class", "project-item-text");
-    ptextcon.appendChild(pspan);
-    pimage.setAttribute("src", project.img);
-    pimage.setAttribute("alt", "No Image Available");
-    pimagecon.setAttribute("class", "flex-container project-item-image");
-    pimagecon.appendChild(pimage);
-    pitem.setAttribute("class", "project-item");
-    pitem.appendChild(pimagecon);
-    pitem.appendChild(ptextcon);
-    projectContainer.appendChild(pitem);
-    console.log(pitem);
-}
-
-//order of elements matter since we select the children based on hard coded index
-//z-index for the projectimage is null when first starting for some reason
-function projectItemOnClick(e){
-    let parent = e.target.parentNode;
-    while(!parent.classList.contains("project-item")){
-        parent = parent.parentNode;
-    }
-    let projectImg = parent.children[0];
-    let projectText = parent.children[1];
-
-    if(!projectImg.style.zIndex){
-        projectImg.style.zIndex = 1;
-    }
-
-    if( projectImg.style.zIndex > projectText.style.zIndex  ){
-        projectText.style.zIndex++;
-        projectImg.style.zIndex--;
-    }
-    else{
-        projectText.style.zIndex--;
-        projectImg.style.zIndex++;
-    }
-}
-
-
 projectContainer.addEventListener('click', function(e){
     if( e.clientX <= projectContainerLeft){
         currentProject = --currentProject % numProjects;
@@ -110,6 +65,84 @@ window.addEventListener("resize", function(){
     computeValues();
 });
 
+//computes values provided that projects are loaded and projects aren't empty 
+function computeValues(){
+    scrollAmountTillNextProject = window.innerWidth - parseInt(style.getPropertyValue("gap"));
+    projectItemWidth = projects[0].offsetWidth;
+    projectContainerLeft = window.innerWidth/2 - projectItemWidth/2 ;
+    projectContainerRight = window.innerWidth/2 + projectItemWidth/2;
+}
+
+function createLinkElement(link, classes){
+    let pLink = document.createElement("a");
+    pLink.setAttribute("href", link);
+    pLink.setAttribute("class", classes);
+    pLink.setAttribute("target", "_blank");   
+    return pLink; 
+}
+
+function addProject(project){
+    let pItem = document.createElement("div");
+    let pImageCon = document.createElement("div");
+    let pImage = document.createElement("img");
+    let pTextCon = document.createElement("div");
+    let pP = document.createElement("p");
+    let pH = document.createElement("h1");
+    let pLinksCon = document.createElement("div");
+
+    pLinksCon.setAttribute("class", "flex-container project-item-links");
+    if( project.codeLink ){
+        pLinksCon.appendChild( createLinkElement(project.codeLink, "fa fa-code") );
+    }
+    if( project.codeLink ){
+        pLinksCon.appendChild( createLinkElement(project.projLink, "fa fa-laptop") );
+    }
+    pP.appendChild(document.createTextNode(project.description));
+    pH.appendChild(document.createTextNode(project.name));
+    pTextCon.setAttribute("class", "project-item-text");
+    pTextCon.appendChild(pH);
+    pTextCon.appendChild(pP);
+    pTextCon.appendChild(pLinksCon);
+
+    pImage.setAttribute("src", project.img);
+    pImage.setAttribute("alt", "No Image Available");
+    pImageCon.setAttribute("class", "flex-container project-item-image");
+    pImageCon.appendChild(pImage);
+    pItem.setAttribute("class", "project-item");
+    pItem.appendChild(pImageCon);
+    pItem.appendChild(pTextCon);
+    projectContainer.appendChild(pItem);
+}
+
+//order of elements matter since we select the children based on hard coded index
+//z-index for the projectimage is null when first starting for some reason
+function projectItemOnClick(e){
+    if( e.target.tagName === "A") return;
+
+    let parent = e.target.parentNode;
+    while(!parent.classList.contains("project-item")){
+        parent = parent.parentNode;
+    }
+    let projectImg = parent.children[0];
+    let projectText = parent.children[1];
+
+    if(!projectImg.style.zIndex){
+        projectImg.style.zIndex = 1;
+    }
+
+    if( projectImg.style.zIndex > projectText.style.zIndex  ){
+        projectText.style.zIndex++;
+        projectImg.style.zIndex--;
+    }
+    else{
+        projectText.style.zIndex--;
+        projectImg.style.zIndex++;
+    }
+}
+
+function uncover(){
+    document.body.style.visibility = "visible";
+}
 
 function initialize() {
     projectList.forEach(addProject);

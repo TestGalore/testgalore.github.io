2d9016f2b2006ae4b27464e0cf098213a47928a7
ElasticBop <elasticbop@gmail.com>
2023-05-12 20:50:23 -0700

    Cleaning up code

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+
+//global vars
+let projectList = [{
+    description : "Response Portfolio. Built using HTML, CSS, and Javascript.",
+    link : "https://elasticbop.github.io/projects/Pathfinding/index.html",
+    img : "./images/project1.PNG"
+}, {
+    description : "A-star Visualizer. Built using HTML, CSS, and Javascript",
+    link : "https://elasticbop.github.io/projects/Pathfinding/index.html",
+    img : "./images/project2.PNG"        
+}
+];
+let projectContainer = document.getElementById("project-container");
+let style = window.getComputedStyle(projectContainer);
+let scrollAmountTillNextProject = window.innerWidth - parseInt(style.getPropertyValue("gap"));
+
+let currentProject = 0;
+let numProjects = 0;
+let projects = null;
+let projectItemWidth = 0;
+let projectContainerLeft = 0;
+let projectContainerRight = 0;
+
 document.addEventListener('DOMContentLoaded', function() {
     window.addEventListener('scroll', function() {
         let scroll = window.scrollY;
@@ -11,37 +34,13 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-let projectList = [{
-    description : "Responsive portfolio to store  my current and future projects. Built using HTML, CSS, and Javascript.",
-    img : "./images/project1.PNG"
-}, {
-    description : "Responsive visualization of the A-star pathfinding algorithm using the manhattan distance as a metric. Built using HTML, CSS, and Javascript",
-    link : "https://elasticbop.github.io/projects/Pathfinding/index.html",
-    img : "./images/project2.PNG"        
-}
-];
-
-let projectContainer = document.getElementById("project-container");
-let projects = document.getElementsByClassName("project-item");
-
-let style = window.getComputedStyle(projectContainer);
-let scrollAmountTillNextProject = window.innerWidth - parseInt(style.getPropertyValue("gap"));
-
-let projectItemWidth = 0;
-
-let projectContainerLeft = window.innerWidth/2 - projectItemWidth/2 ;
-let projectContainerRight = window.innerWidth/2 + projectItemWidth/2;
-let currentProject = 0;
-let numProjects = 2;
-
+//computes values provided that projects are loaded 
 function computeValues(){
-    style = window.getComputedStyle(projectContainer);
     scrollAmountTillNextProject = window.innerWidth - parseInt(style.getPropertyValue("gap"));
     projectItemWidth = projects[0].offsetWidth;
     projectContainerLeft = window.innerWidth/2 - projectItemWidth/2 ;
     projectContainerRight = window.innerWidth/2 + projectItemWidth/2;
 }
-
 
 function addProject(project){
     let pitem = document.createElement("div");
@@ -73,13 +72,17 @@ function projectItemOnClick(e){
     let projectImg = parent.children[0];
     let projectText = parent.children[1];
 
-    if( projectImg.style.zIndex != 2){
-        projectImg.style.zIndex = 2;
-        projectText.style.zIndex = 3;
+    if(!projectImg.style.zIndex){
+        projectImg.style.zIndex = 1;
+    }
+
+    if( projectImg.style.zIndex > projectText.style.zIndex  ){
+        projectText.style.zIndex++;
+        projectImg.style.zIndex--;
     }
     else{
-        projectImg.style.zIndex = 3;
-        projectText.style.zIndex = 2;        
+        projectText.style.zIndex--;
+        projectImg.style.zIndex++;
     }
 }
 
@@ -99,26 +102,23 @@ projectContainer.addEventListener('click', function(e){
         left: scrollAmountTillNextProject*currentProject,
         behaviour: "smooth",
     });
-    console.log(currentProject);
 });
 
 window.addEventListener("resize", function(){
     currentProject = 0;
     projectContainer.scrollLeft = 0;
-    scrollAmountTillNextProject = window.innerWidth - parseInt(style.getPropertyValue("gap"));
-    projectItemWidth = projects[0].offsetWidth;
-    projectContainerLeft = window.innerWidth/2 - projectItemWidth/2;
-    projectContainerRight = window.innerWidth/2 + projectItemWidth/2;
+    computeValues();
 });
 
 
 function initialize() {
     projectList.forEach(addProject);
     projects = document.getElementsByClassName("project-item");
+    numProjects = projects.length;
+    computeValues();
     for( let i = 0; i < projects.length; i++ ){
         projects[i].addEventListener('click', projectItemOnClick)
     }
-    computeValues();
 }
 
 initialize();
